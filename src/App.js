827da7fc8f99cafb4a666ed05c8f56592647927b
Kingsley <kingsleyumujeyan@gmail.com>
2021-02-19 23:55:32 +0100

    ft--form basic validation added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { Button, Form, Container, Header } from "semantic-ui-react";
+import { Button, Form, Container, Header} from "semantic-ui-react";
 import "./App.css";
 
 export default class App extends Component {
@@ -21,6 +21,7 @@ export default class App extends Component {
     e.preventDefault();
     console.log(this.state);
 
+
     const url = "https://sheet.best/api/sheets/71dc919e-ae85-47e4-bd02-a1a925a07e8a";
     axios.post(url, this.state)
     .then(response => {
@@ -39,22 +40,22 @@ export default class App extends Component {
     return (
       <Container fluid className="container">
         <Header as="h2">React Google Sheets!</Header>
-        <Form className="form" onSubmit={this.handleSubmit}>
-          <Form.Field>
+        <Form className="form" onSubmit={this.handleSubmit} error>
+          <Form.Field required>
             <label>Name</label>
-            <input placeholder="Enter your name" type="text" name="name" value={name} onChange={this.handleChange} />
+            <input placeholder="Enter your name" type="text" name="name" value={name} onChange={this.handleChange} required />
           </Form.Field>
-          <Form.Field>
+          <Form.Field required>
             <label>Age</label>
-            <input placeholder="Enter your age" type="text" name="age" value={age} onChange={this.handleChange} />
+            <input placeholder="Enter your age" type="text" name="age" value={age} onChange={this.handleChange}  required />
           </Form.Field>
-          <Form.Field>
+          <Form.Field required>
             <label>Salary</label>
-            <input placeholder="Enter your salary" type="text" name="salary" value={salary} onChange={this.handleChange} />
+            <input placeholder="Enter your salary" type="text" name="salary" value={salary} onChange={this.handleChange}  required />
           </Form.Field>
-          <Form.Field>
+          <Form.Field required>
             <label>Hobby</label>
-            <input placeholder="Enter your hobby" type="text" name="hobby" value={hobby} onChange={this.handleChange} />
+            <input placeholder="Enter your hobby" type="text" name="hobby" value={hobby} onChange={this.handleChange}  required />
           </Form.Field>
           <Button type="submit" >Submit</Button>
         </Form>
